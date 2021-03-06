98020e6ff1cf05c1474d372c1376b93102d4dc64
Petro Manus <petrmanus@gmail.com>
2021-03-06 22:56:32 +0100

    Initial commit. MyWizardTelegramBot class added.

---
+++ src/test/java/com/manus/mywizard_bot/MywizardBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manus.mywizard_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywizardBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
